43929771067e10fd3cf64073443dc29f29e2685e
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-02-14 16:45:58 +0100

    pushing again

--- assets/components/UserDashboard.js
+++ assets/components/UserDashboard.js
@@ -1,24 +1,59 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform, Button} from 'react-native'
 import React from 'react'
 
 import { StatusBar } from 'expo-status-bar'
 import css from './css'
 
+import { useFonts } from 'expo-font'
+
+import { useState } from 'react'
+
 // const DashboardImage = require('../Images/Dashboard-logo-icon.png')
 
 const UserDashboard = () => {
+
+  const [loaded] = useFonts({
+    CustomFont: require('../fonts/ProtestRiot-Regular.ttf'),
+    iosFonts: require('../fonts/ProtestRiot-Regular.ttf'),
+    // Add more fonts if needed
+  });
+
+  if (!loaded) {
+    return null; // You might want to show a loading indicator here
+  }
+
   const design = {
       backgroundColor: 'green',
       height: '20%',
       width: '50%',
       marginVertical:100
     }
+
+    const [isStyled, setIsStyled] = useState(false);
+
+    const toggleStyle = () => {
+      setIsStyled(!isStyled);
+    };
   
   return (
     <View style={styles.container}>
       <View style={[styles.Box, {borderRadius:10}]}></View>
       <View style={design}></View>
       <View style={css}></View>
+      {Platform.OS === 'ios' ? (
+        
+        <Text style={styles.iosFonts}>Hello World</Text>
+        ) : (
+          
+          <Text style={styles.androidFonts}>Hello World</Text>
+      ) }
+
+
+      <View style={[styles.container, isStyled && styles.programmaticallyStyled]}>
+        <Text style={styles.text}>Hello, React Native!</Text>
+        <Button title="Toggle Style" onPress={toggleStyle} />
+      </View>
+
     </View>
   )
 }
@@ -38,6 +73,16 @@ const styles = StyleSheet.create({
       height: '20%',
       width: '50%',
       marginVertical:100
+    },
+
+    androidFonts:{
+      fontFamily:'CustomFont',
+    },
+    iosFonts:{
+      fontFamily:'iosFonts',
+    },
+    programmaticallyStyled:{
+      backgroundColor:'green'
     }
     
 })
